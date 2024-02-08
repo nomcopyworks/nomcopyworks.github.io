14fde2372ac25022b7aea8d6194ed5a2aee543e0
NaramAlhasani <naramalhasani@icloud.com>
2024-02-08 18:15:12 -0500

    added description and cleaned up some things

--- src/layout/CavaniHead.js
+++ src/layout/CavaniHead.js
@@ -3,13 +3,13 @@ const CavaniHead = () => {
   return (
     <Head>
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="description" content="Name of your web site" />
-      <meta name="author" content="Marketify" />
+      <meta name="description" content="Mathew Labar, Visionary footwear designer and creator! Contact for custom inquiries." />
+      <meta name="author" content="Naram Alhasani" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1"
       />
-      <title>Matt LaBar</title>
+      <title>Matt LaBar - Footwear Designer</title>
       {/* STYLES */}
       <link
         href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
